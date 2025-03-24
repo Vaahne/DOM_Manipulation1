b2fbe3ecc1d88b1a9e75f1acd49efd7b61356a77
Spring -Git <vaahne@yahoo.com>
2025-03-24 15:45:10 -0600

    Work

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+// import "../styles/styles.css";
+// import "./styles.css";
+
+let mainEl = document.querySelector("main");
+
+mainEl.style.backgroundColor = "var(--main-bg)";
+
+mainEl.innerHTML = "<h1>DOM Manipulation</h1>";
+mainEl.classList.add("flex-ctr");
+
+let topMenuEl = document.querySelector("#top-menu");
+
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.classList.add("flex-around");
+
+var menuLinks = [
+  { text: "about", href: "/about" },
+  { text: "catalog", href: "/catalog" },
+  { text: "orders", href: "/orders" },
+  { text: "account", href: "/account" },
+];
+
+for (let link of menuLinks) {
+  let anchor = document.createElement("a");
+  anchor.setAttribute("href", link.href);
+  anchor.innerText = link.text;
+  topMenuEl.appendChild(anchor);
+}
